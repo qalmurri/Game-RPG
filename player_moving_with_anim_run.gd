88661e0fb6efac4f9a5f13314acd948e477690f2
qalmurri <86642474+qalmurri@users.noreply.github.com>
2023-06-03 15:02:17 +0700

    Create player_moving_with_anim_run.gd

---
+++ player_moving_with_anim_run.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@export var speed = 60
+@onready var animation_tree : AnimationTree = $AnimationTree
+
+var direction : Vector2 = Vector2.ZERO
+
+func _ready():
+	animation_tree.active = true
+
+func _process(_delta):
+	update_animation_parameters()
+
+func _physics_process(_delta):
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
+func update_animation_parameters():
+	if(velocity == Vector2.ZERO): 
+		animation_tree["parameters/conditions/walk_to_idle"] = true
+		animation_tree["parameters/conditions/idle_to_walk"] = false
+	else:
+		animation_tree["parameters/conditions/walk_to_idle"] = false
+		animation_tree["parameters/conditions/idle_to_walk"] = true
+
+	if(Input.is_action_pressed("ui_accept")):
+		animation_tree["parameters/conditions/walk_to_run"] = true
+		animation_tree["parameters/conditions/run_to_walk"] = false	
+	else:
+		animation_tree["parameters/conditions/run_to_walk"] = true	
+		animation_tree["parameters/conditions/walk_to_run"] = false
+		
+	if(direction != Vector2.ZERO):
+		animation_tree["parameters/Idle/blend_position"] = direction
+		animation_tree["parameters/Walk/blend_position"] = direction
+		animation_tree["parameters/Run/blend_position"] = direction
